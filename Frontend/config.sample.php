bf6644f486307498a1c5184ddbf5e5d70b5f6aaa
iovidius <josjr2009@live.nl>
2022-01-20 00:35:49 +0100

    config samples

---
+++ Frontend/config.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+const API_KEY = '';       // api-key required by backend
+const API_URL = '';       // link to your backend
+const DEFAULT_USER = '';  // 'your-url.com' will show 'your-url.com/default_user' 
+
+?>
