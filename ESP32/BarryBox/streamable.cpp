a7aa5e0c833a1e50378180a68ec939eb545cbce3
Evert Hessel <evert@everthessel.nl>
2022-01-21 20:04:30 +0100

    Removed unnecessary Bootstrap CSS file Fixed indentation and code style

--- ESP32/BarryBox/streamable.cpp
+++ ESP32/BarryBox/streamable.cpp
@@ -2,37 +2,47 @@
 
 #include <Arduino.h>
 
-enum StreamableType {SOUND, STREAM, SAY, NIL};
+enum StreamableType
+{
+  SOUND,
+  STREAM,
+  SAY,
+  NIL
+};
 
 // Object defining soundboard sounds / streams / TTS
-class Streamable {
+class Streamable
+{
 
   StreamableType type;
   String data; // text / sound / stream
   String language;
 
-  public:
-  StreamableType Type(){ return type;}
-  String Data(){return data;}
-  String Language(){return language;}
+public:
+  StreamableType Type() { return type; }
+  String Data() { return data; }
+  String Language() { return language; }
 
-  void SetType(StreamableType type){this->type = type;}
-  void SetData(String data){this->data = data;}
-  void SetData(const char* data){this->data = String(data);}
-  void SetLanguage(String language){this->language = language;}
-  void SetLanguage(const char* language){this->language = String(language);}
+  void SetType(StreamableType type) { this->type = type; }
+  void SetData(String data) { this->data = data; }
+  void SetData(const char *data) { this->data = String(data); }
+  void SetLanguage(String language) { this->language = language; }
+  void SetLanguage(const char *language) { this->language = String(language); }
 
-  Streamable(){
+  Streamable()
+  {
     type = StreamableType::NIL;
   }
 
-  Streamable(StreamableType _type, String _data, String _language = ""){
+  Streamable(StreamableType _type, String _data, String _language = "")
+  {
     type = _type;
     data = _data;
     language = _language;
   }
 
-  Streamable(StreamableType _type, const char* _data, const char* _language=""){
+  Streamable(StreamableType _type, const char *_data, const char *_language = "")
+  {
     type = _type;
     data = String(_data);
     language = String(_language);
--- Frontend/web/js/form.js
+++ Frontend/web/js/form.js
@@ -1,97 +1,95 @@
 $(document).ready(function () {
-	
-  $(".loader").fadeOut("slow");
-	
-	
-  $("form").submit(function (event) {
-	  
-	// disable button for 5s
-	$('#submit').prop('disabled', true);
-	$('.sound-btn').prop('disabled', true);
-		  
-    var formData = {
-      message: $("#message").val(),
-	  language: $("#language").val(),
-	  client: client
-    };
 
-    $.ajax({
-      type: "POST",
-      url: "../../include/process.php",
-      data: formData,
-      dataType: "json",
-      encode: true,
-    }).done(function (data) {
-	  
-      //console.log(data);
-	  
-	  $('#message').val('');
-	  $(".help-block").remove();
-	  $(".alert-success").remove();
-	  $("#message-group").removeClass("has-error");
-	  
-	  // Validation
-	  if (!data.success) {
-		  
-		$('#submit').prop('disabled', false);
-		$('.sound-btn').prop('disabled', false);
-		
-        if (data.errors.message) {
-          $("#message-group").addClass("has-error");
-          $("#message-group").append(
-            '<div class="help-block">' + data.errors.message + "</div>"
-          );
-        }
-		
-		if (data.errors.language){
-		  $("#lang-group").addClass("has-error");
-          $("#lang-group").append(
-            '<div class="help-block">' + data.errors.language + "</div>"
-          );
-		}
+	$(".loader").fadeOut("slow");
 
-      } else {
-		
-        $("#firstrow").append(
-          '<div class="alert alert-success alert-dismissible fade show" style="margin-left:5px;width:98%;" role="alert">' + data.result +
-		  '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-        );
-		
-		 setTimeout(function() {
-			   $('#submit').prop('disabled', false);
-			   $('.sound-btn').prop('disabled', false);
-		 }, 5000);
-		}
-    });
 
-    event.preventDefault();
-  }),
-  $('.sound-btn').click(function () {
-	  
+	$("form").submit(function (event) {
+
 		// disable button for 5s
 		$('#submit').prop('disabled', true);
 		$('.sound-btn').prop('disabled', true);
-		
-		var val = $(this).val();
+
+		var formData = {
+			message: $("#message").val(),
+			language: $("#language").val(),
+			client: client
+		};
+
 		$.ajax({
-			type: 'POST',
-			url: '../../include/sound.php',
-			data: {
-				sound: val,
-				client: client
-			},
-		dataType : 'json'
-		}).always(function (data) {
-			
-			 setTimeout(function() {
-				   $('#submit').prop('disabled', false);
-				   $('.sound-btn').prop('disabled', false);
-			 }, 5000);
-		 
+			type: "POST",
+			url: "../../include/process.php",
+			data: formData,
+			dataType: "json",
+			encode: true,
+		}).done(function (data) {
+
 			//console.log(data);
-			
+
+			$('#message').val('');
+			$(".help-block").remove();
+			$(".alert-success").remove();
+			$("#message-group").removeClass("has-error");
+
+			// Validation
+			if (!data.success) {
+
+				$('#submit').prop('disabled', false);
+				$('.sound-btn').prop('disabled', false);
+
+				if (data.errors.message) {
+					$("#message-group").addClass("has-error");
+					$("#message-group").append(
+						'<div class="help-block">' + data.errors.message + "</div>"
+					);
+				}
+
+				if (data.errors.language) {
+					$("#lang-group").addClass("has-error");
+					$("#lang-group").append(
+						'<div class="help-block">' + data.errors.language + "</div>"
+					);
+				}
+
+			} else {
+
+				$("#firstrow").append(
+					'<div class="alert alert-success alert-dismissible fade show" style="margin-left:5px;width:98%;" role="alert">' + data.result +
+					'<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+				);
+
+				setTimeout(function () {
+					$('#submit').prop('disabled', false);
+					$('.sound-btn').prop('disabled', false);
+				}, 5000);
+			}
 		});
-	})
+
+		event.preventDefault();
+	}),
+		$('.sound-btn').click(function () {
+
+			// disable button for 5s
+			$('#submit').prop('disabled', true);
+			$('.sound-btn').prop('disabled', true);
+
+			var val = $(this).val();
+			$.ajax({
+				type: 'POST',
+				url: '../../include/sound.php',
+				data: {
+					sound: val,
+					client: client
+				},
+				dataType: 'json'
+			}).always(function (data) {
+
+				setTimeout(function () {
+					$('#submit').prop('disabled', false);
+					$('.sound-btn').prop('disabled', false);
+				}, 5000);
+
+				//console.log(data);
+
+			});
+		})
 });
-
-
